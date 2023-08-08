6eceebc180dfc29fecec474c2a6bb14fd2265c63
CodeNoGorD <code.nogord@gmail.com>
2023-08-08 12:19:50 +0200

    Ãmodifications project

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,16 +6,35 @@ use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class QuestionController extends AbstractController
 {
-    #[Route('/api/up', name: 'app_question_up')]
-    public function up(Question $question, SerializerInterface $serializer, QuestionRepository $qr, EntityManagerInterface $em): Response
+    #[Route('/api/up', name: 'app_question_up', methods: ['PATCH'])]
+    public function up(Request $request, QuestionRepository $qr, EntityManagerInterface $em): Response
     {
-        $id = json_decode($qr->getId());
-        return $id;
+        $requete = json_decode($request->getContent());
+        $id = $requete->{"id"};
+
+        $nbr = $qr->find($id)->getScore() + 1;
+        $em->persist($qr->find($id)->setScore($nbr));
+        $em->flush();
+
+        return $this->json(['message' => 'Le score a été incrémenté']);
+    }
+    #[Route('/api/down', name: 'app_question_down', methods: ['PATCH'])]
+    public function down(Request $request, QuestionRepository $qr, EntityManagerInterface $em): Response
+    {
+        $requete = json_decode($request->getContent());
+        $id = $requete->{"id"};
+
+        $nbr = $qr->find($id)->getScore() - 1;
+        $em->persist($qr->find($id)->setScore($nbr));
+        $em->flush();
+
+        return $this->json(['message' => 'Le score a été décrementé']);
     }
 }
